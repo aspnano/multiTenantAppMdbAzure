516b20a18b50121acb11a03edeb909dbacb97c46
Ryan <rlsystems@outlook.com>
2024-08-29 11:45:08 -0500

    Reorg tenant service

--- multiTenantApp/Services/TenantService/TenantService.cs
+++ multiTenantApp/Services/TenantService/TenantService.cs
@@ -15,7 +15,7 @@ namespace multiTenantApp.Services.TenantService
     public class TenantService : ITenantService
     {
 
-        private readonly BaseDbContext _baseDbContext; 
+        private readonly BaseDbContext _baseDbContext;
         private readonly IConfiguration _configuration;
         private readonly IServiceProvider _serviceProvider;
         private readonly IWebHostEnvironment _environment;
@@ -46,75 +46,66 @@ namespace multiTenantApp.Services.TenantService
             };
 
 
-            // create a new tenant database and bring current with any pending migrations from ApplicationDbContext
             try
             {
-                _ = await _baseDbContext.Tenants.AddAsync(tenant);
-
                 if (request.Isolated == true)
                 {
-                    try
+                    // create a new tenant database and bring current with any pending migrations from ApplicationDbContext
+                    IServiceScope scopeTenant = _serviceProvider.CreateScope();
+                    ApplicationDbContext applicationDbContext = scopeTenant.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                    applicationDbContext.Database.SetConnectionString(modifiedConnectionString);
+
+                    if (_environment.IsDevelopment()) // localhost code
                     {
-                        IServiceScope scopeTenant = _serviceProvider.CreateScope();
-                        ApplicationDbContext applicationDbContext = scopeTenant.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-                        applicationDbContext.Database.SetConnectionString(modifiedConnectionString);
-
-                        if (_environment.IsDevelopment()) // localhost code
+                        if (applicationDbContext.Database.GetPendingMigrations().Any())
                         {
-                            if (applicationDbContext.Database.GetPendingMigrations().Any())
-                            {
-                                Console.ForegroundColor = ConsoleColor.Blue;
-                                Console.WriteLine($"Applying ApplicationDB Migrations for New '{request.Id}' tenant.");
-                                Console.ResetColor();
-                                await applicationDbContext.Database.MigrateAsync();
-                            }
-                        }
-                        else // azure code
-                        {
-                            IConfigurationSection azureConfig = _configuration.GetSection("Azure");
-                            DefaultAzureCredential credential = new();
-                            ArmClient armClient = new(credential, azureConfig["SubscriptionId"]);
-                            Azure.Response<SqlServerResource> sqlServer = await armClient.GetResourceGroupResource(
-                                    new ResourceIdentifier(
-                                        $"/subscriptions/{azureConfig["SubscriptionId"]}/resourceGroups/{azureConfig["ResourceGroupName"]}"))
-                                .GetAsync()
-                                .Result
-                                .Value.GetSqlServerAsync(azureConfig["SqlServerName"]);
-
-                            // Create a new database in the Elastic Pool
-                            ArmOperation<SqlDatabaseResource> databaseResponse = await sqlServer.Value
-                                .GetSqlDatabases().CreateOrUpdateAsync(WaitUntil.Completed,
-                                    tenantDbName,
-                                    new SqlDatabaseData(new AzureLocation(sqlServer.Value.Data.Location))
-                                    {
-                                        ElasticPoolId =
-                                            (await sqlServer.Value.GetElasticPoolAsync(
-                                                azureConfig["ElasticPoolName"]))
-                                            .Value.Data.Id
-                                    });
-
-                            // apply migrations to the new database
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine($"Applying ApplicationDB Migrations for New '{request.Id}' tenant.");
+                            Console.ResetColor();
                             await applicationDbContext.Database.MigrateAsync();
-                            if (databaseResponse.Value.Data.Id == null)
-                            {
-                                throw new Exception("Couldnt create DB on azure");
-                            }
                         }
                     }
-                    catch (Exception ex)
+                    else // azure code
                     {
-                        throw new Exception(ex.Message);
+                        IConfigurationSection azureConfig = _configuration.GetSection("Azure");
+                        DefaultAzureCredential credential = new();
+                        ArmClient armClient = new(credential, azureConfig["SubscriptionId"]);
+                        Azure.Response<SqlServerResource> sqlServer = await armClient.GetResourceGroupResource(
+                                new ResourceIdentifier(
+                                    $"/subscriptions/{azureConfig["SubscriptionId"]}/resourceGroups/{azureConfig["ResourceGroupName"]}"))
+                            .GetAsync()
+                            .Result
+                            .Value.GetSqlServerAsync(azureConfig["SqlServerName"]);
+
+                        // Create a new database in the Elastic Pool
+                        ArmOperation<SqlDatabaseResource> databaseResponse = await sqlServer.Value
+                            .GetSqlDatabases().CreateOrUpdateAsync(WaitUntil.Completed,
+                                tenantDbName,
+                                new SqlDatabaseData(new AzureLocation(sqlServer.Value.Data.Location))
+                                {
+                                    ElasticPoolId =
+                                        (await sqlServer.Value.GetElasticPoolAsync(
+                                            azureConfig["ElasticPoolName"]))
+                                        .Value.Data.Id
+                                });
+
+                        // apply migrations to the new database
+                        await applicationDbContext.Database.MigrateAsync();
+                        if (databaseResponse.Value.Data.Id == null)
+                        {
+                            throw new Exception("Couldnt create DB on azure");
+                        }
                     }
                 }
+
+                // apply changes to base db context
+                _ = await _baseDbContext.Tenants.AddAsync(tenant); // save tenant info
+                _ = await _baseDbContext.SaveChangesAsync();
             }
             catch (Exception ex)
             {
                 throw new Exception(ex.Message);
             }
-
-
-
-            _ = await _baseDbContext.SaveChangesAsync(); // save tenant info to baseDb
 
 
             return tenant;
